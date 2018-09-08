2d3c6586e367f11ae380cbca9fd9c64eeb981efc
coozin <coozin@coozin-ubuntu.frontierlocal.net>
2018-09-08 13:37:48 -0400

    added material ui and custom toolbar

---
+++ src/components/Menu.js
@@ -0,0 +1,46 @@
+import React from 'react';
+// import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+import MenuIcon from '@material-ui/icons/Menu';
+
+class SimpleMenu extends React.Component {
+  state = {
+    anchorEl: null,
+  };
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { anchorEl } = this.state;
+
+    return (
+      <div>
+        <MenuIcon
+          aria-owns={anchorEl ? 'simple-menu' : null}
+          aria-haspopup="true"
+          onClick={this.handleClick}
+        />
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.handleClose}
+        >
+          <MenuItem onClick={this.handleClose}>Tickers</MenuItem>
+          <MenuItem onClick={this.handleClose}>Trading</MenuItem>
+          <MenuItem onClick={this.handleClose}>Orderbook</MenuItem>
+        </Menu>
+      </div>
+    );
+  }
+}
+
+export default SimpleMenu;
